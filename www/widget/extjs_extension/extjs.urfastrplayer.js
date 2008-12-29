e4474ced3453923fcc535e3b217596822e25588e
Jerome Etienne <jerome.etienne@gmail.com>
2008-12-29 22:02:30 +0100

    - added extjs and jquery widget 	new file:   extjs_extension/demo.html 	new file:   extjs_extension/extjs.urfastrplayer.js 	new file:   jquery_plugin/demo.html 	new file:   jquery_plugin/jquery-1.2.6.min.js 	new file:   jquery_plugin/jquery.urfastrplayer.js

---
+++ www/widget/extjs_extension/extjs.urfastrplayer.js
@@ -0,0 +1,20 @@
+/*
+ * UrFastR Player extjs extension
+ *
+ * Copyright (c) 2008 Jerome Etienne (urfastr.net)
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ * - this is a ultra early version made while experimenting with extjs
+ */
+
+Ext.ux.UrFastRPlayerWindow = Ext.extend(Ext.Window, {
+	title		: '<center><a href="http://urfastr.net/video/player">UrFastsR Player</a></center>',
+	layout		: 'fit',
+	width		: 320,
+	height		: 240,
+	closeAction	:'close',
+	plain		: true,
+	html		: "<iframe src='http://player.urfastr.tv/live' "
+				+ "frameborder='no' height='100%' width='100%'></iframe>",
+});
---
+++ www/widget/jquery_plugin/jquery.urfastrplayer.js
@@ -0,0 +1,22 @@
+/*
+ * UrFastR Player jquery plugin
+ *
+ * Copyright (c) 2008 Jerome Etienne (urfastr.net)
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ * - this is a ultra early version made while experimenting with jquery
+ */
+
+jQuery.fn.UrFastR_Player = function(options) {
+	var settings = jQuery.extend({
+		width: 	320,
+		height:	240
+	}, options);
+	iframeEl	= document.createElement('iframe');
+	iframeEl.setAttribute('src'		, 'http://player.urfastr.tv/live');
+	iframeEl.setAttribute('width'		, settings.width);
+	iframeEl.setAttribute('height'		, settings.height);
+	iframeEl.setAttribute('frameborder'	, 'no');	
+	return this.empty().append(iframeEl);
+};
