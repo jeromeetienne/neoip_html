d2ac7e2272e7e75077a9693a84690d95ccc89552
Jerome Etienne <jerome.etienne@gmail.com>
2009-02-04 04:59:52 +0100

    - more work on adobe_air/adobe_air2 widget 	modified:   adobe_air/urfastr-player.air 	modified:   adobe_air2/Makefile 	modified:   adobe_air2/application.xml 	new file:   adobe_air2/applicationupdater_ui.swf 	new file:   adobe_air2/html/index.html 	renamed:    adobe_air2/src/index.html -> adobe_air2/html/player_window.html 	modified:   adobe_air2/js/navigation.js 	deleted:    adobe_air2/src/application.xml 	deleted:    adobe_air2/src/cert.pfx 	deleted:    adobe_air2/src/images/thumbnail-128.png 	deleted:    adobe_air2/src/images/thumbnail-16.png 	deleted:    adobe_air2/src/images/thumbnail-32.png 	deleted:    adobe_air2/src/images/thumbnail-48.png 	deleted:    adobe_air2/src/images/thumbnail.png 	new file:   adobe_air2/update.xml 	modified:   adobe_air2/urfastr-player.air

--- www/widget/adobe_air2/js/navigation.js
+++ www/widget/adobe_air2/js/navigation.js
@@ -39,6 +39,41 @@ function gotoPicInPic()
 	nativeWindow.bounds	= bounds;	
 }
 
+/********************************************************************************/
+/********************************************************************************/
+/*		appUpdater Stuff							*/
+/********************************************************************************/
+/********************************************************************************/
+
+function appUpdaterCheckUpdate() {
+	air.trace("air updater checkupdate");	
+	var updater	= new runtime.air.update.ApplicationUpdaterUI();
+	// we set the URL for the update.xml file
+	updater.updateURL = "http://urfastr.net/static/player/widget/adobe_air2/update.xml";
+	// we set the event handlers for INITIALIZED nad ERROR
+	updater.addEventListener(runtime.air.update.events.UpdateEvent.INITIALIZED	, appUpdaterOnUpdate);
+	updater.addEventListener(runtime.flash.events.ErrorEvent.ERROR			, appUpdaterOnError);
+	// we can hide the dialog asking for permission for checking for a new update;
+	// if you want to see it just leave the default value (or set true).
+	updater.isCheckForUpdateVisible	= false;
+	// if isFileUpdateVisible is set to true, File Update, File No Update, 
+	// and File Error dialog boxes will be displayed
+	updater.isFileUpdateVisible	= false;
+	// if isInstallUpdateVisible is set to true, the dialog box for installing the update is visible
+	updater.isInstallUpdateVisible	= false;
+	// we initialize the updater
+	updater.initialize();
+}
+			
+function appUpdaterOnUpdate(event) {
+	var updater	= new runtime.air.update.ApplicationUpdaterUI();
+	//starts the update process
+	updater.checkNow();
+}
+
+function appUpdaterOnError(event) {
+	alert(event);
+}
 
 /********************************************************************************/
 /********************************************************************************/
@@ -47,26 +82,44 @@ function gotoPicInPic()
 /********************************************************************************/
 
 var winPlayerLoader	= null;
-function winPlayerOpen()
+function winPlayerOpen(options)
 {
 	// if it is already open, return now
 	if( winPlayerLoader )	return;
-	// get options for the 
-	var options		= new air.NativeWindowInitOptions();
-	options.systemChrome	= air.NativeWindowSystemChrome.NONE;
-	//options.transparent	= true;
+	// handle the default options
+	options		= options || {	'chrome': 	false,
+					'position':	'bottomright',
+					'size':		'small',
+					'stayInFront':	true
+					};
+	// get winopts for the window
+	var winopts		= new air.NativeWindowInitOptions();
 
-	var win_w	= 320*2/3;
-	var win_h	= 240*2/3;
-	var bounds	= new air.Rectangle(air.Capabilities.screenResolutionX - win_w,
-					    air.Capabilities.screenResolutionY - win_h,
-					    win_w, win_h);
+	if(options.chrome == false){
+		winopts.systemChrome	= air.NativeWindowSystemChrome.NONE;
+		winopts.transparent	= true;
+	}
+	if(options.size	== "small"){
+		var win_w	= 320*2/3;
+		var win_h	= 240*2/3;
+	}else if(options.size	== "medium"){
+		var win_w	= 320*2;
+		var win_h	= 240*2;
+	}
+	if(options.position == "center"){
+		var win_x	= (air.Capabilities.screenResolutionX - win_w)/2;
+		var win_y	= (air.Capabilities.screenResolutionY - win_h)/2;
+	}else if(options.position == "bottomright"){
+		var win_x	= air.Capabilities.screenResolutionX - win_w;
+		var win_y	= air.Capabilities.screenResolutionY - win_h;		
+	}
 
 	// create a new window
-	loader = air.HTMLLoader.createRootWindow( true, options, true, bounds );
+	var bounds	= new air.Rectangle(win_x, win_y,win_w, win_h);	
+	loader		= air.HTMLLoader.createRootWindow( true, winopts, true, bounds );
 	// set it always in front
-	loader.stage.nativeWindow.alwaysInFront	= true;	
-        loader.load( new air.URLRequest('player_window.html') );
+	if(options.stayInFront)	loader.stage.nativeWindow.alwaysInFront	= true;	
+        loader.load( new air.URLRequest('html/player_window.html') );
 	loader.stage.nativeWindow.activate();
 	
 	winPlayerLoader	= loader;
@@ -86,16 +139,39 @@ function winPlayerIsOpened()
 }
 
 
-
+/********************************************************************************/
+/********************************************************************************/
+/*		TO COMMENT							*/
+/********************************************************************************/
+/********************************************************************************/
 
 function myOnLoadCallback()
 {
+	//nativeWindow.visible	= true;
+
 	systrayInit();
+	air.trace("post systray init");
+	
+	// set the startAtLogin
+	// - it is known to fails thru adl
+	try{
+		air.NativeApplication.nativeApplication.startAtLogin = true;
+	}catch(e){air.trace(e);	}
+
+	
+	//setTimeout(appUpdaterCheckUpdate, 1000);
+	
+	//air.navigateToURL(new air.URLRequest('http://google.com'));
 }
 window.onload	= myOnLoadCallback;
 
 
 
+/********************************************************************************/
+/********************************************************************************/
+/*		systray stuff							*/
+/********************************************************************************/
+/********************************************************************************/
 function systrayInit()
 {
 	// Loader to load the icon image
@@ -104,21 +180,14 @@ function systrayInit()
 	
 	// Handle when the icon image is loaded
 	// Load the icon image (in this case local)
-	loader.contentLoaderInfo.addEventListener(air.Event.COMPLETE, doLoaderComplete );
+	loader.contentLoaderInfo.addEventListener(air.Event.COMPLETE, systrayLoaderOnComplete );
 	loader.load( new air.URLRequest("images/thumbnail-48.png") );
-}
-
-// Called when the dock menu item to close is selected
-// Calls a shared function to close the window
-function doExitSelect( evt )
-{
-	closeApplication();		
 }
 
 
 // Called when the icon image is loaded
 // Setup systray/dock actions depending on operating system
-function doLoaderComplete( evt )
+function systrayLoaderOnComplete( evt )
 {
 	var isMac = null;
 	
@@ -139,7 +208,7 @@ function doLoaderComplete( evt )
 	}
 
 	// Setup a menu on the docked icon to restore or close
-	var	myMenu	= createMenu( isMac );
+	var	myMenu	= NativeMenuCreate( isMac );
 	air.NativeApplication.nativeApplication.icon.menu = myMenu;
 }
 
@@ -152,41 +221,57 @@ function systrayOnClick( evt )
 	else				winPlayerOpen();
 }
 
-// Called to close the window
-// Shared by multiple event handlers
-function closeApplication()
-{
-	winPlayerClose();
-	nativeWindow.close();	
-}
 
 // Called with the dock item to open the window is selected
 // Calls a shared function to open (restore) the window
-function doAboutSelect( evt )
+function NativeMenuOnSelect( evt )
 {
-	winPlayerOpen();
+	// log to debug
+	air.trace("Selected command: PRE"); 
+	air.trace("Selected command: " + evt.target.label); 
+	air.trace("Selected command: POST"); 
+
+	if( evt.target.label == "Open" ){
+		winPlayerClose();
+		winPlayerOpen({	'chrome': 	true,
+				'position':	'center',
+				'size':		'medium',
+				'stayInFront':	false
+				})
+	}else if( evt.target.label == "About" ){
+		var url	= 'http://urfastr.net';
+		air.navigateToURL(new air.URLRequest(url));
+	}else if( evt.target.label == "Quit" ){
+		winPlayerClose();
+		nativeWindow.close();	
+	}
 }
 
 // Called to create a menu on the systray/dock icon
 // Takes operating system into consideration
-function createMenu( isMac )
+function NativeMenuCreate( isMac )
 {
 	var menu	= new air.NativeMenu();
-	
-	// Both operating systems have an option to open the window
-	var openItem	= new air.NativeMenuItem("About" );
-	openItem.addEventListener(air.Event.SELECT, doAboutSelect );	
-	menu.addItem( openItem );
+	var menuItem	= null;
+
+	// install the menuitem 'open'
+	menuItem	= new air.NativeMenuItem("Open" );
+	menuItem.addEventListener(air.Event.SELECT, NativeMenuOnSelect );	
+	menu.addItem( menuItem );
+	// install the menuitem 'about'
+	menuItem	= new air.NativeMenuItem("About" );
+	menuItem.addEventListener(air.Event.SELECT, NativeMenuOnSelect );	
+	menu.addItem( menuItem );
 	
 	if( !isMac ){
-		var exitItem	= null;
 		// Mac provides built-in "Quit" item
 		// Create an "Exit" item for Windows
-		var menuItem	= new air.NativeMenuItem("Quit");
-		menuItem.addEventListener(air.Event.SELECT, closeApplication );	
+		// install the menuitem 'quit'
+		menuItem	= new air.NativeMenuItem("Quit");
+		menuItem.addEventListener(air.Event.SELECT, NativeMenuOnSelect );	
 		menu.addItem( menuItem );
 	}
-
+	// return the menu itself
 	return menu;	
 }
 
