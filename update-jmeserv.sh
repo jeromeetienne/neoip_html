cad413c62cc1ca01a175bfcf283d637c1ff24c2f
Jerome Etienne <jerome.etienne@gmail.com>
2009-03-29 16:01:49 +0200

    - admin stuff 	new file:   .gitmodules 	modified:   jspackmin/data/neoip_jsplayer_packmin.js 	modified:   update-jmeserv.sh

--- update-jmeserv.sh
+++ update-jmeserv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # do a mirror on the current directory into jmeserv:neoip_html directory
-rsync -z -v -a --rsh='ssh' --stats ~/workspace/yavipin/html/ dedixl:neoip_html
+rsync -z -v -a --exclude .git --rsh='ssh' --stats ~/webwork/neoip_html/ dedixl:neoip_html
 
 # update the directory from dbg environment to rel one
 ssh dedixl "cd neoip_html/player; rm -f cache; ln -sf cache_rel cache"
