7a3e1e21af9c04ae9ae7f2618ecf612c1c438fb5
Jerome Etienne <jerome.etienne@gmail.com>
2009-05-10 18:41:46 +0200

    - urfastr_live.js: made player_url tunable 	modified:   player/api/urfastr_live.js

--- player/api/urfastr_live.js
+++ player/api/urfastr_live.js
@@ -1,10 +1,14 @@
 /**
  * this object is part of the javascript API for UrFastR Live
  * - opt.container: the html id of the container
+ *
+ * NOTE: it MUST not depends on other libraries
+ * - it has an optionnal dependancy on crossframe library
 */
 var urfastr_live = function(opt){
 	// set the default options
 	var opt_dfl 	= {
+			'player_url':	'http://player.urfastr.net/live',
 			'width':	"320",
 			'height':	"240",
 			'neoip_var_arr':{
@@ -22,9 +26,10 @@ var urfastr_live = function(opt){
 			opt[key]	= opt_dfl[key];
 		}
 		// init the rpc_client
+		// TODO make this dependancy optionnal
 		rpc_client	= new crossframe.rpc_client_t({
 			dest_addr: {
-				proxyUrl:	"http://player.urfastr.tv/crossframe_proxy.html",
+				proxyUrl:	"http://player.urfastr.net/crossframe_proxy.html",
 				//proxyUrl:	"http://localhost/~jerome/neoip_html/lib/crossframe/crossframe_proxy.html",
 				listener_obj:	"crossframe_msg",
 				iframe_dst:	"frames['"+frame_id+"']"
@@ -41,23 +46,22 @@ var urfastr_live = function(opt){
 	 * Build the dom element for UrFastR Player inside opt.container_id
 	*/
 	var build	= function(){
-		var iframe_src	= "http://player.urfastr.tv/live";
-		//iframe_src	= "http://localhost/~jerome/neoip_html/bt_cast/casto/neoip_casto_dev.html";
-
 		// convert opt.neoip_var_arr into a list of url variable
 		var var_str	= ""
 		for(key in opt.neoip_var_arr){
 			if( var_str.length )	var_str += "&";
 			var_str	+= 'neoip_var_'+ escape(key) + '=' + escape(opt.neoip_var_arr[key]);
 		}
+
+		// build iframe_src
+		var iframe_src	= opt.player_url;
 		if(var_str.length)	iframe_src	+= '?' + var_str;
-		
 		
 		// build the body
 		var iframeEl	= document.createElement('iframe');
-		iframeEl.setAttribute('src'		, iframe_src);
-		iframeEl.setAttribute('width'		, opt.width);
-		iframeEl.setAttribute('height'		, opt.height);
+		iframeEl.setAttribute('src'		, iframe_src	);
+		iframeEl.setAttribute('width'		, opt.width	);
+		iframeEl.setAttribute('height'		, opt.height	);
 		iframeEl.setAttribute('frameborder'	, 'no'		);
 		iframeEl.setAttribute('frameborder'	, 'no'		);
 		iframeEl.setAttribute('id'		, frame_id	);
