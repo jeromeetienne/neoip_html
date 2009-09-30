1bc782402e81cd3848ce1d63b3d736f22b39d3ce
Jerome Etienne <jerome.etienne@gmail.com>
2009-10-01 01:12:12 +0200

    - player/jsplayer: better naming for files 	renamed:    player/jsplayer/neoip_ezplayer_widget_iframe.html -> player/jsplayer/neoip_ezplayer_widget_playground.html - player/jsplayer: fixed little bug with the onload_start_play 	modified:   player/jsplayer/neoip_ezplayer_widget_t.js 	modified:   player/jsplayer/slota_rel_live.html

--- player/jsplayer/neoip_ezplayer_widget_t.js
+++ player/jsplayer/neoip_ezplayer_widget_t.js
@@ -103,7 +103,7 @@ neoip.ezplayer_widget_t.prototype._parse_cfgvar_arr	= function()
 {
 	var urivar_arr	= neoip.core.doc_urivar_arr();
 	var cfgvar_arr	= {
-		//onload_start_play	: "enabled",
+		onload_start_play	: "enabled",
 		fullpage_state		: 'maximized'
 	};
 	for(var key in urivar_arr){
